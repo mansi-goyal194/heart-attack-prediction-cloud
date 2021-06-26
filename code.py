69387f51eaa717322cbc5a972fbf4bab151a4b94
rads2710 <61345984+rads2710@users.noreply.github.com>
2021-06-26 23:41:53 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,17 @@
 #import libraries
 import pandas as pd
 import numpy as np
+import pickle 
 #import matplotlib.pyplot as plt
 #from sklearn import *
 #import data
 df = pd.read_csv('cleveland.csv')
-
+#te = pd.read_csv('input.csv')
 #Adding columns
 df.columns = ["age", "sex", "cp", "restbp", "chol", "fbs", "restecg", 
            "thalach", "exang", "oldpeak", "slope", "ca", "thal", "target"]
-                                               
+#te.columns = ["age", "sex", "cp", "restbp", "chol", "fbs", "restecg", 
+#           "thalach", "exang", "oldpeak", "slope", "ca", "thal"]                                             
 
 #print(df.to_string())
 df['target'] = df.target.map({0: 0, 1: 1, 2: 1, 3: 1, 4: 1})
@@ -26,16 +28,18 @@ y = df.iloc[:, -1].values
 #Seprating train and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
+print(X_train)
+print(" -----------------------------------------")
 #Standard scaling data
-from sklearn.preprocessing import StandardScaler as ss
-sc = ss()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
+from sklearn.preprocessing import StandardScaler 
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+#pickle.dump(scaler, open('scaler.pkl','wb'))
 
 #Logistic Regression Algorithm
 from sklearn.linear_model import LogisticRegression
+
 log_reg = LogisticRegression()
 
 log_reg.fit(X_train, y_train)
@@ -45,9 +49,24 @@ print(y_test)
 print(log_reg.score(X_train,y_train))
 print(log_reg.score(X_test, y_test))
 
-#y_pred_log_reg = log_reg.predict(X_test)
+y_pred_log_reg = log_reg.predict(X_test)
+print(y_pred_log_reg)
+#print(te)
+check = np.array([[49,1,2,130,266,0,0,171,0,0.6,1,0,3]])
+#sc = pickle.load(open('scaler.pkl','rb'))
+print(scaler.mean_)
+for i in range(12):
+    print(check[0][i])
+    check[0][i] = check[0][i] - scaler.mean_[i]
+print("---------------------------------------------")
+print(check)    
+#s = scaler.fit_transform(check)
+#print(s)
 
-print(log_reg.predict([[56,1,2,130,221,0,2,163,0,0,1,0,7]]))
+
+# = sc.fit_transform(te)
+#print(te)
+print(log_reg.predict(check))
 #from sklearn.neighbors import KNeighborsClassifier
 
 #knn=KNeighborsClassifier(n_neighbors=29)
@@ -60,5 +79,5 @@ print(log_reg.predict([[56,1,2,130,221,0,2,163,0,0,1,0,7]]))
 
 
 #print(knn.predict([[58,0,4,130,197,0,0,131,0,0.6,2,0,3]]))
-import pickle 
+
 pickle.dump(log_reg,open('log_model.pkl','wb'))
